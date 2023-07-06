bd43b979cf517d71aa7d26df2569566096095c42
litongjava <litongjava@qq.com>
2023-07-06 19:42:47 +0800

    封装完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
 <template>
-<div id="app">
-  <router-view></router-view>
-</div>
+<router-view></router-view>
 </template>
-
-<script>
-
-export default {
-  name: 'app',
-  components: {
-  }
-}
-</script>
-
-<style>
-</style>
